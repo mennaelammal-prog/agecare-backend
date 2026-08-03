dc0737441fdf31ffe22bb7267b50a1d17cbaf6e3
mennaelammal-prog <mennaelammal@gmail.com>
2026-08-03 13:01:06 +0800

    Clean JavaScript backend - SQLite, bcrypt auth, no TypeScript

--- dist/routes/familyLink.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post('/', async (req, res) => {
-    res.json({ success: true, message: 'Family link feature placeholder' });
-});
-exports.default = router;
-//# sourceMappingURL=familyLink.routes.js.map
